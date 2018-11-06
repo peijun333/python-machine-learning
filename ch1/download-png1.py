e1a7a85d48da43e9374deb54cd0d33c108227b0a
peijun333 <Kzyun0205>
2018-11-07 06:59:14 +0900

    第1章クローリングとスクレイピング

---
+++ ch1/download-png1.py
@@ -0,0 +1,11 @@
+#ライブラリの取り込む
+import urllib.request
+
+#URLと保存パスを指定
+url = "http://uta.pw/shodou/img/28/214.png"
+savename = "test.png"
+
+#ダウンロード
+urllib.request.urlretrieve(url,savename)
+print("保存しました")
+
